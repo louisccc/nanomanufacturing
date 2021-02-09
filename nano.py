020f802bab86248c17da6f7f5814078498f02715
Fangzhou <dufangzhou99@gmail.com>
2021-02-09 11:40:19 -0800

    csv added.

--- nano.py
+++ nano.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from argparse import ArgumentParser, RawTextHelpFormatter
 import os
 import sys
+import csv
 
 class Config:
     '''Configuration and Argument Parser for particle detection.'''
@@ -18,7 +19,7 @@ class Config:
         self.parser.add_argument('--max_radius', type=int, default=50, help='Maximum radius for particles being detected.')
         self.parser.add_argument('--output_path_1', type=str, default='output1.txt', help="Output path for detected results in zone 1")
         self.parser.add_argument('--output_path_2', type=str, default='output2.txt', help="Output path for detected results in zone 2")
-
+        self.parser.add_argument('--smooth_box_size', type=int, default=5, help="Size of the averaging box used for smoothing through 1D Convlution")
         args_parsed = self.parser.parse_args(args)
         for arg_name in vars(args_parsed):
             self.__dict__[arg_name] = getattr(args_parsed, arg_name)
@@ -33,6 +34,7 @@ class ParticleDetector:
         self.sampling_interval = cfg.sampling_interval
         self.min_radius = cfg.min_radius
         self.max_radius = cfg.max_radius
+        self.smooth_box_size = cfg.smooth_box_size
 
         self.path1 = cfg.output_path_1
         self.path2 = cfg.output_path_2
@@ -98,22 +100,47 @@ class ParticleDetector:
             for a,b,r in bags_0+bags_1:
                 cv2.circle(img, (a, b), r, (0, 255, 0), 2) # Draw the circumference of the circle. 
                 cv2.circle(img, (a, b), 1, (0, 0, 255), 3) # Draw a small circle (of radius 1) to show the center. 
-            # cv2.imwrite(str(detected_image_folder_path / image_path.name), img)
+            cv2.imwrite(str(detected_image_folder_path / image_path.name), img)
 
             ''' calculate features for each bead '''
             features_bag_0 = self.get_features_for_bag(bags_0, 2, 3)
             features_bag_1 = self.get_features_for_bag(bags_1, 4, 5)
 
-            if features_bag_0 == (-1, -1, -1, -1, -1, 0):
+            if features_bag_0 == [-1, -1, -1, -1, -1, 0]:
                 print("detecting beads having issues in zone 0: %s" % image_path)
-            if features_bag_1 == (-1, -1, -1, -1, -1, 0):
+            if features_bag_1 == [-1, -1, -1, -1, -1, 0]:
                 print("detecting beads having issues in zone 1: %s" % image_path)
 
-            feature_group_0.append((int(image_path.name.split('.')[0]), features_bag_0))
-            feature_group_1.append((int(image_path.name.split('.')[0]), features_bag_1))
+            feature_group_0.append([int(image_path.name.split('.')[0]), features_bag_0])
+            feature_group_1.append([int(image_path.name.split('.')[0]), features_bag_1])
 
             print("Particle detecting %s successful." % str(image_path))
         
+        # sort result by frame number
+        feature_group_0 = sorted(feature_group_0, key=lambda x: x[0])
+
+        # # remove invalid data
+        # for i in range(len(feature_group_0)):
+        #     if feature_group_0[i][1] == [-1, -1, -1, -1, -1, 0]:
+        #         if i != 0:
+        #             feature_group_0[i][1] = feature_group_0[i - 1][1] 
+        #         else:
+        #             feature_group_0[i][1] = feature_group_0[i + 1][1] 
+        #     # import pdb; pdb.set_trace()
+
+        avg_box = 5
+        for i in range(len(feature_group_0)):
+            if feature_group_0[i][1] == [-1, -1, -1, -1, -1, 0]:
+                if i >= avg_box:
+                    for avg_idx in range(5):
+                        avg_sum = 0
+                        for avg_count in range(avg_box):
+                            avg_sum = avg_sum + feature_group_0[i - avg_count - 1][1][avg_idx]
+                        avg_sum = avg_sum / avg_box
+                        feature_group_0[i][1][avg_idx] = round(avg_sum, 3)
+                else:
+                    feature_group_0[i][1] = feature_group_0[i + 1][1]
+
         return feature_group_0, feature_group_1
         
     def draw_bars(self):
@@ -138,6 +165,25 @@ class ParticleDetector:
             pprint.pprint(feature_bag_0, file1)
             pprint.pprint(feature_bag_1, file2)
 
+    def store_to_csv_files(self, path1, path2, feature_bag_0, feature_bag_1):
+        with open(path1, 'w') as csvfile1:
+            spamwriter = csv.writer(csvfile1)
+            spamwriter.writerow(["Frames","x_avr" ,"x_std"," avg_norm_x","avg_norm_abs_x","std_norm_abs_x","num_beads"])
+            for rows in feature_bag_0:
+                temp_list = [rows[0]]
+                temp_list[1:]= [i for i in rows[1]]
+                # temp_list.insert(0, rows[0])
+                spamwriter.writerow(temp_list)
+
+        with open(path2, 'w') as csvfile2:
+            spamwriter = csv.writer(csvfile1)
+            spamwriter.writerow(["Frames","x_avr" ,"x_std"," avg_norm_x","avg_norm_abs_x","std_norm_abs_x","num_beads"])
+            for rows in feature_bag_1:
+                temp_list = [rows[0]]
+                temp_list[1:]= [i for i in rows[1]]
+                # temp_list.insert(0, rows[0])
+                spamwriter.writerow(temp_list)
+
     def get_features_for_bag(self, bag, bar1, bar2):
         if bar1 == 2 and bar2 == 3:
             reference = self.bar2[0][0] + self.bar3[1][0]
@@ -156,10 +202,21 @@ class ParticleDetector:
                        
             num_beads = len(bag)
 
-            return (x_avr, x_std, avg_norm_x, avg_norm_abs_x, std_norm_abs_x, num_beads)
+            return [x_avr, x_std, avg_norm_x, avg_norm_abs_x, std_norm_abs_x, num_beads]
         except: 
-            return (-1, -1, -1, -1, -1, 0)
+            return [-1, -1, -1, -1, -1, 0]
 
+    def smooth_convolve(self, feature_bag):
+        # feature_bag = sorted(feature_bag, key=lambda x: x[0])
+        for smooth_idx in range(5):
+            smooth_list = []
+            for feature in feature_bag:
+                smooth_list.append(feature[1][smooth_idx])
+            box = np.ones(self.smooth_box_size)/self.smooth_box_size
+            smooth_output = np.convolve(smooth_list, box, mode='same')
+            for i in range(int(self.smooth_box_size/2), int(len(feature_bag)-(self.smooth_box_size/2))):
+                feature_bag[i][1][smooth_idx] = round(smooth_output[i],3)
+        return feature_bag
 
 def run_video_10k20v():
     ''' 
@@ -180,7 +237,9 @@ def run_video_10k20v():
     detector.convert_video_to_images(max_f_num=6300)
     # detector.draw_bars()
     features_0, features_1 = detector.read_analyze_images()
-    detector.store_to_txt_files("./10k20v_output1.txt", "./10k20v_output2.txt", features_0, features_1)
+    # smooth_features_0 = detector.smooth_convolve(features_0)
+    # detector.store_to_txt_files("./10k20v_output1.txt", "./10k20v_output2.txt", features_0, features_1)
+    detector.store_to_csv_files("./10k20v_output1.csv", "./10k20v_output2.csv", features_0, features_1)
 
 def run_video_5mhz5v():
     ''' 
@@ -204,7 +263,29 @@ def run_video_5mhz5v():
     features_0, features_1 = detector.read_analyze_images()
     detector.store_to_txt_files("./5mhz5v_output1.txt", "./5mhz5v_output2.txt", features_0, features_1)
 
+def run_video_2mhz1v():
+    ''' 
+        only capture particles in the following zones: between bar2 and bar3, between bar4 and bar5.
+    '''
+    cfg = Config(sys.argv[1:])
+    detector = ParticleDetector(cfg)
+    detector.bar1 = [(50, 95), (50, 750)]
+    detector.bar2 = [(285, 5), (285, 750)]
+    detector.bar3 = [(570, 5), (570, 750)]
+    detector.bar4 = [(770, 5), (770, 750)]
+    detector.bar5 = [(970, 5), (970, 750)]
+    detector.video_path = "./2mhz1v.avi"		
+    detector.image_path = "./2mhz1v_extracted_frame"		
+    detector.bar_image_path = "./2mhz1v_bar_frame"		
+    detector.detected_image_path = "./2mhz1v_result_frame"
+
+    detector.convert_video_to_images(max_f_num=6300)
+    # detector.draw_bars()
+    features_0, features_1 = detector.read_analyze_images()
+    # smooth_features_0 = detector.smooth_convolve(features_0)
+    detector.store_to_txt_files("./2mhz1v_output1.txt", "./2mhz1v_output2.txt", features_0, features_1)
 
 if __name__ == "__main__":
-    run_video_10k20v()
+    # run_video_10k20v()
     # run_video_5mhz5v()
+    run_video_2mhz1v()
