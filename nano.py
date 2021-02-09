1c604d17ab2eabd61f322d78f91a368174d8f687
Fangzhou <dufangzhou99@gmail.com>
2021-02-09 15:36:23 -0800

    Code refactoring. Parameter2 decreased from 30 to 18

--- nano.py
+++ nano.py
@@ -16,10 +16,11 @@ class Config:
         self.parser.add_argument('--detected_image_path', type=str, default='./detected_output', help="Output path for video to image convertion")
         self.parser.add_argument('--sampling_interval', type=int, default=30, help="Take one frame for every sampling interval.")
         self.parser.add_argument('--min_radius', type=int, default=1, help='Minimum radius for particles being detected.')
-        self.parser.add_argument('--max_radius', type=int, default=50, help='Maximum radius for particles being detected.')
+        self.parser.add_argument('--max_radius', type=int, default=30, help='Maximum radius for particles being detected.')
         self.parser.add_argument('--output_path_1', type=str, default='output1.txt', help="Output path for detected results in zone 1")
         self.parser.add_argument('--output_path_2', type=str, default='output2.txt', help="Output path for detected results in zone 2")
-        self.parser.add_argument('--smooth_box_size', type=int, default=5, help="Size of the averaging box used for smoothing through 1D Convlution")
+        self.parser.add_argument('--smooth_box_size', type=int, default=-1, help="Size of the averaging box used for smoothing through 1D Convlution")
+        self.parser.add_argument('--invalid_box_size', type=int, default=5, help="Size of the averaging box used for invalid data replacement")
         args_parsed = self.parser.parse_args(args)
         for arg_name in vars(args_parsed):
             self.__dict__[arg_name] = getattr(args_parsed, arg_name)
@@ -35,6 +36,7 @@ class ParticleDetector:
         self.min_radius = cfg.min_radius
         self.max_radius = cfg.max_radius
         self.smooth_box_size = cfg.smooth_box_size
+        self.invalid_box_size = cfg.invalid_box_size
 
         self.path1 = cfg.output_path_1
         self.path2 = cfg.output_path_2
@@ -46,7 +48,7 @@ class ParticleDetector:
     
         # create a empty folder 
         root_folder_path = Path(self.image_path)
-        root_folder_path.mkdir(exist_ok=True)
+        root_folder_path.mkdir(parents=True, exist_ok=True)
         
         # capture images in avi file according to the framerate.
         for frame_no in range(0, max_frame_no, self.sampling_interval):
@@ -60,8 +62,10 @@ class ParticleDetector:
 
     def read_analyze_images(self):
 
+        INVALID_DATA = [-1, -1, -1, -1, -1, 0]
+
         detected_image_folder_path = Path(self.detected_image_path)
-        detected_image_folder_path.mkdir(exist_ok=True)
+        detected_image_folder_path.mkdir(parents=True, exist_ok=True)
         
         feature_group_0 = []
         feature_group_1 = []
@@ -75,10 +79,9 @@ class ParticleDetector:
             kernel = np.ones((5,5),np.uint8)
             img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            gray_blurred = cv2.blur(gray, (3,3))
 
-            detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1,20, param1=50,param2=30,minRadius=self.min_radius, maxRadius=self.max_radius)
-            detected_circles = np.uint16(np.around(detected_circles)) 
+            detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1,20, param1=50,param2=18,minRadius=self.min_radius, maxRadius=self.max_radius)
+            detected_circles = np.uint16(np.around(detected_circles))
             
             ''' filtering the detected circles out of area of interests '''
             for pt in detected_circles[0, :]: 
@@ -92,23 +95,23 @@ class ParticleDetector:
                     bags_1.append((a,b,r))
 
             ''' draw bars and detected circles '''
-            cv2.line(img, self.bar1[0], self.bar1[1], (0, 255, 0))
-            cv2.line(img, self.bar2[0], self.bar2[1], (0, 255, 0))
-            cv2.line(img, self.bar3[0], self.bar3[1], (0, 255, 0))
-            cv2.line(img, self.bar4[0], self.bar4[1], (0, 255, 0))
-            cv2.line(img, self.bar5[0], self.bar5[1], (0, 255, 0))    
+            cv2.line(gray, self.bar1[0], self.bar1[1], (0, 255, 0))
+            cv2.line(gray, self.bar2[0], self.bar2[1], (0, 255, 0))
+            cv2.line(gray, self.bar3[0], self.bar3[1], (0, 255, 0))
+            cv2.line(gray, self.bar4[0], self.bar4[1], (0, 255, 0))
+            cv2.line(gray, self.bar5[0], self.bar5[1], (0, 255, 0))    
             for a,b,r in bags_0+bags_1:
-                cv2.circle(img, (a, b), r, (0, 255, 0), 2) # Draw the circumference of the circle. 
-                cv2.circle(img, (a, b), 1, (0, 0, 255), 3) # Draw a small circle (of radius 1) to show the center. 
-            cv2.imwrite(str(detected_image_folder_path / image_path.name), img)
+                cv2.circle(gray, (a, b), r, (0, 255, 0), 2) # Draw the circumference of the circle. 
+                cv2.circle(gray, (a, b), 1, (0, 0, 255), 3) # Draw a small circle (of radius 1) to show the center. 
+            cv2.imwrite(str(detected_image_folder_path / image_path.name), gray)
 
             ''' calculate features for each bead '''
             features_bag_0 = self.get_features_for_bag(bags_0, 2, 3)
             features_bag_1 = self.get_features_for_bag(bags_1, 4, 5)
 
-            if features_bag_0 == [-1, -1, -1, -1, -1, 0]:
+            if features_bag_0 == INVALID_DATA:
                 print("detecting beads having issues in zone 0: %s" % image_path)
-            if features_bag_1 == [-1, -1, -1, -1, -1, 0]:
+            if features_bag_1 == INVALID_DATA:
                 print("detecting beads having issues in zone 1: %s" % image_path)
 
             feature_group_0.append([int(image_path.name.split('.')[0]), features_bag_0])
@@ -118,31 +121,30 @@ class ParticleDetector:
         
         # sort result by frame number
         feature_group_0 = sorted(feature_group_0, key=lambda x: x[0])
-
-        # # remove invalid data
-        # for i in range(len(feature_group_0)):
-        #     if feature_group_0[i][1] == [-1, -1, -1, -1, -1, 0]:
-        #         if i != 0:
-        #             feature_group_0[i][1] = feature_group_0[i - 1][1] 
-        #         else:
-        #             feature_group_0[i][1] = feature_group_0[i + 1][1] 
-        #     # import pdb; pdb.set_trace()
-
-        avg_box = 5
-        for i in range(len(feature_group_0)):
-            if feature_group_0[i][1] == [-1, -1, -1, -1, -1, 0]:
-                if i >= avg_box:
-                    for avg_idx in range(5):
-                        avg_sum = 0
-                        for avg_count in range(avg_box):
-                            avg_sum = avg_sum + feature_group_0[i - avg_count - 1][1][avg_idx]
-                        avg_sum = avg_sum / avg_box
-                        feature_group_0[i][1][avg_idx] = round(avg_sum, 3)
-                else:
-                    feature_group_0[i][1] = feature_group_0[i + 1][1]
-
+        feature_group_1 = sorted(feature_group_1, key=lambda x: x[0])
+        feature_group_0 = self.invalid_data_replacement(feature_group_0)
+        feature_group_1 = self.invalid_data_replacement(feature_group_1)
         return feature_group_0, feature_group_1
+    
+    def invalid_data_replacement(self, features):
+        INVALID_DATA = [-1, -1, -1, -1, -1, 0]
+        num_feature = len(features[0][1])
+        features_return = features
+        for i in range(self.config.invalid_box_size):
+            if features_return[i][1] == INVALID_DATA:
+                features_return[i][1] = features_return[i + 1][1]
+
+        for i in range(self.config.invalid_box_size, len(features_return)):
+            if features_return[i][1] == INVALID_DATA:
+                for avg_idx in range(num_feature):
+                    avg_sum = 0
+                    for avg_count in range(self.config.invalid_box_size):
+                        avg_sum = avg_sum + features_return[i - avg_count - 1][1][avg_idx]
+                    avg_sum = avg_sum / self.config.invalid_box_size
+                    features_return[i][1][avg_idx] = round(avg_sum, 3)
         
+        return features_return 
+
     def draw_bars(self):
         ''' draw bars and stores the resulting images in bar_image_folder_path (for debug) '''
         bar_image_folder_path = Path(self.bar_image_path)
@@ -157,14 +159,6 @@ class ParticleDetector:
             cv2.line(img, self.bar5[0], self.bar5[1], (0, 255, 0))
             cv2.imwrite(str(bar_image_folder_path / image_path.name), img)
 
-    def store_to_txt_files(self, path1, path2, feature_bag_0, feature_bag_1):
-        feature_bag_0 = sorted(feature_bag_0, key=lambda x: x[0])
-        feature_bag_1 = sorted(feature_bag_1, key=lambda x: x[0])
-        
-        with open(path1, 'w') as file1, open(path2, 'w') as file2:
-            pprint.pprint(feature_bag_0, file1)
-            pprint.pprint(feature_bag_1, file2)
-
     def store_to_csv_files(self, path1, path2, feature_bag_0, feature_bag_1):
         with open(path1, 'w') as csvfile1:
             spamwriter = csv.writer(csvfile1)
@@ -176,7 +170,7 @@ class ParticleDetector:
                 spamwriter.writerow(temp_list)
 
         with open(path2, 'w') as csvfile2:
-            spamwriter = csv.writer(csvfile1)
+            spamwriter = csv.writer(csvfile2)
             spamwriter.writerow(["Frames","x_avr" ,"x_std"," avg_norm_x","avg_norm_abs_x","std_norm_abs_x","num_beads"])
             for rows in feature_bag_1:
                 temp_list = [rows[0]]
@@ -230,16 +224,15 @@ def run_video_10k20v():
     detector.bar4 = [(936, 95), (936, 900)]
     detector.bar5 = [(1160, 95), (1160, 900)]
     detector.video_path = "./10k20v.avi"		
-    detector.image_path = "./10k20v_extracted_frame"		
-    detector.bar_image_path = "./10k20v_bar_frame"		
-    detector.detected_image_path = "./10k20v_result_frame"
+    detector.image_path = "./10k20v/extracted_frame"		
+    detector.bar_image_path = "./10k20v/bar_frame"		
+    detector.detected_image_path = "./10k20v/result_frame"
 
     detector.convert_video_to_images(max_f_num=6300)
-    # detector.draw_bars()
+    detector.draw_bars()
     features_0, features_1 = detector.read_analyze_images()
     # smooth_features_0 = detector.smooth_convolve(features_0)
-    # detector.store_to_txt_files("./10k20v_output1.txt", "./10k20v_output2.txt", features_0, features_1)
-    detector.store_to_csv_files("./10k20v_output1.csv", "./10k20v_output2.csv", features_0, features_1)
+    detector.store_to_csv_files("./10k20v/output1.csv", "./10k20v/output2.csv", features_0, features_1)
 
 def run_video_5mhz5v():
     ''' 
@@ -261,7 +254,7 @@ def run_video_5mhz5v():
     detector.convert_video_to_images()
     detector.draw_bars()
     features_0, features_1 = detector.read_analyze_images()
-    detector.store_to_txt_files("./5mhz5v_output1.txt", "./5mhz5v_output2.txt", features_0, features_1)
+    detector.store_to_csv_files("./5mhz5v_output1.csv", "./5mhz5v_output2.csv", features_0, features_1)
 
 def run_video_2mhz1v():
     ''' 
@@ -269,23 +262,47 @@ def run_video_2mhz1v():
     '''
     cfg = Config(sys.argv[1:])
     detector = ParticleDetector(cfg)
-    detector.bar1 = [(50, 95), (50, 750)]
-    detector.bar2 = [(285, 5), (285, 750)]
-    detector.bar3 = [(570, 5), (570, 750)]
-    detector.bar4 = [(770, 5), (770, 750)]
-    detector.bar5 = [(970, 5), (970, 750)]
+    detector.bar1 = [(35, 5), (35, 800)]
+    detector.bar2 = [(290, 5), (290, 800)]
+    detector.bar3 = [(550, 5), (550, 800)]
+    detector.bar4 = [(805, 5), (805, 800)]
+    detector.bar5 = [(970, 5), (970, 800)]
     detector.video_path = "./2mhz1v.avi"		
-    detector.image_path = "./2mhz1v_extracted_frame"		
-    detector.bar_image_path = "./2mhz1v_bar_frame"		
-    detector.detected_image_path = "./2mhz1v_result_frame"
+    detector.image_path = "./2mhz1v/extracted_frame"		
+    detector.bar_image_path = "./2mhz1v/bar_frame"		
+    detector.detected_image_path = "./2mhz1v/result_frame"
 
-    detector.convert_video_to_images(max_f_num=6300)
-    # detector.draw_bars()
+    detector.convert_video_to_images()
+    detector.draw_bars()
     features_0, features_1 = detector.read_analyze_images()
     # smooth_features_0 = detector.smooth_convolve(features_0)
-    detector.store_to_txt_files("./2mhz1v_output1.txt", "./2mhz1v_output2.txt", features_0, features_1)
+    detector.store_to_csv_files("./2mhz1v/output1.csv", "./2mhz1v/output2.csv", features_0, features_1)
+
+def run_video_20k_1v_layercage():
+    ''' 
+        only capture particles in the following zones: between bar2 and bar3, between bar4 and bar5.
+    '''
+    cfg = Config(sys.argv[1:])
+    detector = ParticleDetector(cfg)
+    detector.bar1 = [(40, 0), (40, 765)]
+    detector.bar2 = [(40, 0), (40, 765)]
+    detector.bar3 = [(285, 0), (285, 765)]
+    detector.bar4 = [(550, 0), (550, 765)]
+    detector.bar5 = [(800, 0), (800, 765)]
+    detector.video_path = "./20k_1v_layercage.avi"		
+    detector.image_path = "./20k_1v_layercage_extracted_frame"		
+    detector.bar_image_path = "./20k_1v_layercage_bar_frame"		
+    detector.detected_image_path = "./20k_1v_layercage_result_frame"
+
+    detector.convert_video_to_images()
+    detector.draw_bars()
+    features_0, features_1 = detector.read_analyze_images()
+    # smooth_features_0 = detector.smooth_convolve(features_0)
+    detector.store_to_csv_files("./20k_1v_output1.csv", "./2mhz1v_output2.csv", features_0, features_1)
 
 if __name__ == "__main__":
-    # run_video_10k20v()
+    run_video_10k20v()
     # run_video_5mhz5v()
     run_video_2mhz1v()
+    run_video_20k_1v_layercage()
+
